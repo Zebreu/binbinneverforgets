f9c31ec5eeb7b18bccc71f9dbd1586b7faebb717
Zebreu <sebouel@gmail.com>
2020-12-26 22:56:59 -0500

    Add more events to the sample file and make calendar query upcoming items

--- source/backend.py
+++ source/backend.py
@@ -2,9 +2,7 @@ import sqlite3
 import os
 
 import pandas as pd
-from flask import Flask
-from flask import request
-
+from flask import Flask, request, json
 
 app = Flask(__name__)
 
@@ -36,11 +34,31 @@ def generate_upcoming_tasks(merged):
 
     schedules = []
     for i, row in merged.iterrows():
-        schedule = pd.date_range(row['date_checked'], today+pd.Timedelta(52, 'W'), freq=f'{row["frequency"]*7}D')
+        schedule = pd.date_range(row['date_checked'], today+pd.Timedelta(13, 'W'), freq=f'{row["frequency"]*7}D')
         schedule = schedule[schedule >= today]
         schedules.append((row['item'],schedule))
 
     return schedules
+
+
+def inspect_inventory_log():
+    """Gathers observations and master data."""
+    today = pd.Timestamp.today()
+    
+    connection = sqlite3.connect(os.path.join(working_directory,'database.db'))
+    checks = pd.read_sql('SELECT * from inventory_log', con = connection)
+    checks['date'] = pd.to_datetime(checks['date'])
+    checks = checks.sort_values('date')
+    last_checked = checks.groupby(['item']).last().reset_index()
+
+    master_data = pd.read_sql('SELECT * from master_data', con = connection)
+    recent_master_data = master_data.sort_values('date_added').groupby('item').last().reset_index()
+    
+    merged = recent_master_data.merge(last_checked, on='item', suffixes=('_initial','_checked'))
+    merged['week_difference'] = (today - merged['date_checked']).dt.days/7
+    merged['need_to_check'] = merged['week_difference'] > merged['frequency']
+
+    return merged 
 
 
 @app.route('/')
@@ -51,7 +69,6 @@ def hello_world():
 @app.route('/read_master_data')
 def read_master_data(inventory_checked = True):
     """Updates a master table from an input file."""
-    
     today = pd.Timestamp.today()
 
     df = pd.read_csv(os.path.join(working_directory, 'horaire_data.csv'))
@@ -67,32 +84,30 @@ def read_master_data(inventory_checked = True):
     return df.to_json(orient='split', index=False)
 
 
+@app.route('/upcoming_items')
+def get_upcoming_items():
+    """Creates a schedule ahead of time."""
+    merged = inspect_inventory_log()
+    schedules = generate_upcoming_tasks(merged)
+    
+    return_values = []
+    for schedule in schedules:
+        title = schedule[0]
+        events = schedule[1]
+        for event in events:
+            item = dict()
+            item['title'] = title
+            item['date'] = event.strftime(format='%Y-%m-%d')
+            return_values.append(item)
+
+    return json.jsonify(return_values)
+
+
 @app.route('/create_report')
 def create_report():
-    """Creates a report of upcoming checks."""
-
-    today = pd.Timestamp.today()
+    """Creates a report of due checks."""
+    merged = inspect_inventory_log()    
     
-    connection = sqlite3.connect(os.path.join(working_directory,'database.db'))
-    checks = pd.read_sql('SELECT * from inventory_log', con = connection)
-    checks['date'] = pd.to_datetime(checks['date'])
-    checks = checks.sort_values('date')
-    last_checked = checks.groupby(['item']).last().reset_index()
-
-    master_data = pd.read_sql('SELECT * from master_data', con = connection)
-    recent_master_data = master_data.sort_values('date_added').groupby('item').last().reset_index()
-    
-    merged = recent_master_data.merge(last_checked, on='item', suffixes=('_initial','_checked'))
-    merged['week_difference'] = (today - merged['date_checked']).dt.days/7
-    merged['need_to_check'] = merged['week_difference'] > merged['frequency']
-    print(merged)
-    
-    schedules = generate_upcoming_tasks(merged)
-    heatmap = create_heatmap(schedules)
-
-    print(schedules)
-    print(heatmap)
-
     merged.insert(0, 'id', pd.RangeIndex(len(merged)))
     merged['need_to_check'] = merged['need_to_check'].astype(str)
 
