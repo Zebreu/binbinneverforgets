70de1dac88dbcb31b88c574f046fc3fc33407555
Vincent Breault <vbreault@inteloom.com>
2021-01-18 09:38:20 +0900

    Removed unused import.

--- simpleone/src/components/datatable.js
+++ simpleone/src/components/datatable.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, componentDidUpdate} from 'react';
+import React, {useState, useEffect } from 'react';
 import { Box, Button } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
 
